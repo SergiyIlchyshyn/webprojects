d6e2feaace60911509887feb000905632f2c999d
SergiyIlchyshyn <sergiy.ilchyshyn@gmail.com>
2020-06-17 16:22:16 +0300

    updated wavy landing

--- Header/Wavy Landing page in HTML CSS JS/main.js
+++ Header/Wavy Landing page in HTML CSS JS/main.js
@@ -1,1 +1,8 @@
-const triggerMenu = document.querySelector('nenu-trigger');
+const triggerMenu = document.querySelector('.menu-trigger');
+
+function handleNavigation() {
+    const body = document.querySelector('body');
+    body.classList.toggle('is-navigation-open');
+}
+
+triggerMenu.addEventListener('click', handleNavigation);
